98f7493c64923b4b3ec81d965d75b3adde235dc2
Khairul <Khairulimran6810@gmail.com>
2023-12-05 10:42:39 +0800

    First commit.

---
+++ src/main/java/vttp/ssf/Day17Lecture/Controller/HomeController.java
@@ -0,0 +1,13 @@
+package vttp.ssf.Day17Lecture.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "/home")
+public class HomeController {
+
+    
+
+    
+}
---
+++ src/main/java/vttp/ssf/Day17Lecture/Day17LectureApplication.java
@@ -0,0 +1,13 @@
+package vttp.ssf.Day17Lecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day17LectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day17LectureApplication.class, args);
+	}
+
+}
